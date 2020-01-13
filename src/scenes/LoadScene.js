f147caa0ed2c3c2e46dcb4d2643cc722fe7c4859
Kevin <Atnnetworkk@gmail.com>
2020-01-13 22:01:03 +0100

    Add the preload function

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -6,6 +6,24 @@ export class LoadScene extends Phaser.Scene{
             key: CST.SCENE.LOAD
         })
     }
+    preload()
+    {
+        this.load.image("background","./assets/img/menu/background.png");
+        this.load.image("play","./assets/img/menu/play.png");
+        this.load.image("settings","./assets/img/menu/settings.png");
+        this.load.image("title","./assets/img/menu/title.png");
+
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff
+            }
+        })
+
+        this.load.on('progress', (percent)=>{
+            loadingBar.fillRect(0,this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
+            console.log(percent)
+        })
+    }
     create()
     {
         this.scene.start(CST.SCENE.MENU)
